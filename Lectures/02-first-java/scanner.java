cd8b1d5ea9fdedc5a094fe9ecde6b53e0393a433
ror loom <hopmin2014@gmail.com>
2022-12-18 18:14:09 +0300

    second practise added

---
+++ Lectures/02-first-java/scanner.java
@@ -0,0 +1,16 @@
+// simple program takes input from user
+import java.util.Scanner;
+public class scanner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(" please Enter your name !");
+        String name = input.nextLine();
+        System.out.println("your name is " + name );
+
+        // float marks = input.nextFloat();
+        //System.out.println(marks); */
+
+       //  int rollno = input.nextInt();
+       //  System.out.println(rollno);
+    }
+}
---
+++ Lectures/02-first-java/swap.java
@@ -0,0 +1,12 @@
+public class swap {
+    public static void main(String[] args) {
+        int a=10;
+        int b= 15;
+       // swap numbres code
+        int temp =a;
+        a=b;
+        b=temp;
+        System.out.println(a + " " + b);
+
+    }
+}
