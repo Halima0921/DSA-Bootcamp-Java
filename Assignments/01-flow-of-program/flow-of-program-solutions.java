26f2f984421b3c944a2b7b8669c7c4010d95c1a4
ror loom <hopmin2014@gmail.com>
2022-12-18 23:07:09 +0300

    solutions and questions added

---
+++ Assignments/01-flow-of-program/flow-of-program-solutions.java
@@ -0,0 +1,19 @@
+Q1: Input a year and find whether it is a leap year or not.
+
+start
+//input year
+if year %4||100 =0
+output leap year
+end
+
+Q2: Take two numbers and print the sum of both.
+
+start
+//input num1
+num1=
+//input num2
+num2=
+sum=num1+num2
+output sum
+end
+
