296e67750f22f5420b2d8dc2df65cbd91ae524d8
ror loom <hopmin2014@gmail.com>
2022-12-19 20:30:04 +0300

    sorting solutions added

---
+++ Assignments/07-sorting/sorting-solutions.java
@@ -0,0 +1,95 @@
+// Easy :
+// Q : [Missing Number](https://leetcode.com/problems/missing-number/)
+
+class Solution {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 0, 1};
+        System.out.println(missingNumber(arr));
+    }
+
+    public static int missingNumber(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+
+        // case 2
+        return arr.length;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
+
+// medium 
+// Q : [Find the Duplicate Number](https://leetcode.com/problems/find-the-duplicate-number/)
+public class FindDuplicate {
+    public int findDuplicate(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != i + 1) {
+                int correct = arr[i] - 1;
+                if (arr[i] != arr[correct]) {
+                    swap(arr, i , correct);
+                } else {
+                    return arr[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
+
+// Q : [Find all Duplicates in an array](https://leetcode.com/problems/find-all-duplicates-in-an-array/)
+class Solution {
+    public List<Integer> findDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index+1) {
+                ans.add(arr[index]);
+            }
+        }
+
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
