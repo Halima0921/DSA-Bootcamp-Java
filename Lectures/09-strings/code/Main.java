1e5e75507a6b49732150a937563d9baeb35e872c
ror loom <hopmin2014@gmail.com>
2022-12-19 16:09:14 +0300

    string practises added

---
+++ Lectures/09-strings/code/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Halima Abdullahi";
+//        System.out.println(name);
+
+        String a = "Halima";
+        System.out.println(a);
+        a = "Abdullahi";
+        System.out.println(a);
+    }
+}
---
+++ Lectures/09-strings/code/Operators.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println("a" + "b");
+        System.out.println((char)('a' + 3));
+
+        System.out.println("a" + 1);
+        // this is same as after a few steps: "a" + "1"
+        // integer will be converted to Integer that will call toString() ex / println()
+
+       // System.out.println("Halima" + new ArrayList<>());
+       // System.out.println("Halima" + new Integer(56));
+        //String ans = new Integer(56) + "" + new ArrayList<>();
+       // System.out.println(ans);
+
+       // System.out.println("a" + 'b');
+    }
+}
