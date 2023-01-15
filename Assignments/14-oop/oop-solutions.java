7a253c62b367f2174ba98107b91f044c744a793a
ror loom <hopmin2014@gmail.com>
2023-01-15 18:32:58 +0300

    oop questions and solutions  added

---
+++ Assignments/14-oop/oop-solutions.java
@@ -0,0 +1,171 @@
+
+2-
+class Arithmetic {
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+
+}
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Adder a = new Adder();
+
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+
+        System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+    }
+}
+
+3-
+
+abstract class Book {
+    String title;
+
+    abstract void setTitle(String s);
+
+    String getTitle() {
+        return title;
+    }
+
+}
+
+
+class MyBook extends Book {
+    void setTitle(String s) {
+        title = s;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: " + new_novel.getTitle());
+        sc.close();
+
+    }
+}
+
+
+4-
+
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int sum = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i == n / i) {
+                    sum += i;
+                } else {
+                    sum += i + n / i;
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+
+
+    static void ImplementedInterfaceNames(Object o) {
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++) {
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
+
+6- 
+class BiCycle {
+    String define_me() {
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle {
+    String define_me() {
+        return "a cycle with an engine.";
+    }
+
+    MotorCycle() {
+        System.out.println("Hello I am a motorcycle, I am " + define_me());
+
+        String temp = super.define_me();// Fix this line
+
+        System.out.println("My ancestor is a cycle who is " + temp);
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) {
+        MotorCycle M = new MotorCycle();
+    }
+}
+
+7-
+
+class Student {
+}
+
+class Rockstar {
+}
+
+class Hacker {
+}
+
+public class InstanceOFTutorial {
+
+    static String count(ArrayList mylist) {
+        int a = 0, b = 0, c = 0;
+        for (int i = 0; i < mylist.size(); i++) {
+            Object element = mylist.get(i);
+            if (element instanceof Student)
+                a++;
+            if (element instanceof Rockstar)
+                b++;
+            if (element instanceof Hacker)
+                c++;
+        }
+        String ret = Integer.toString(a) + " " + Integer.toString(b) + " " + Integer.toString(c);
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        ArrayList mylist = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            String s = sc.next();
+            if (s.equals("Student"))
+                mylist.add(new Student());
+            if (s.equals("Rockstar"))
+                mylist.add(new Rockstar());
+            if (s.equals("Hacker"))
+                mylist.add(new Hacker());
+        }
+        System.out.println(count(mylist));
+    }
+}
